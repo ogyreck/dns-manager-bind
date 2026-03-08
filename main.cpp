626abc4bb980b58b5b1f521f776da48890cdc7dd
sa <sa@astra>
2026-03-08 18:11:40 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "dns-manager-admin_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+
+    a.setApplicationName("dns-mamger-admin");
+    a.setApplicationVersion("1.0.0");
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
